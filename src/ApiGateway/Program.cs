61d28c666b91d8d9cc6398e66fdeaa9e11f6eac2
Burgyn <mino.martiniak@gmail.com>
2020-07-28 14:17:23 +0200

    first commit

---
+++ src/ApiGateway/Program.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace ApiGateway
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureAppConfiguration((hostingContext, config) =>
+                {
+                    config
+                        .SetBasePath(hostingContext.HostingEnvironment.ContentRootPath)
+                        .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+                        .AddJsonFile("ocelot.json", optional: true, reloadOnChange: true)
+                        .AddJsonFile($"appsettings.local.json", optional: true, reloadOnChange: true)
+                        .AddEnvironmentVariables();
+                })
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>()
+                    .UseUrls("http://0.0.0.0:5000");
+                });
+    }
+}
---
+++ src/Service/Controllers/InvoicesController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Service.Controllers
+{
+    [Route("{companyId}/api/[controller]")]
+    [ApiController]
+    public class InvoicesController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Get(int companyId)
+            => Ok(Enumerable.Range(0, 10).Select(i => $"Invoice {i} for company {companyId}"));
+
+        [HttpGet("{invoiceId}")]
+        public ActionResult<string> Get(int companyId, int invoiceId)
+            => $"Invoice {invoiceId} for company {companyId}";
+    }
+}
